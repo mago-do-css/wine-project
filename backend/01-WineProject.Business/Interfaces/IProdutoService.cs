df59ca21d02f61965652d6f01f522c6fa9a98bce
Iago Melo <iagooliveiradev@outlook.com>
2024-08-28 01:07:30 -0300

    feat: criação da interface do produto

---
+++ backend/01-WineProject.Business/Interfaces/IProdutoService.cs
@@ -0,0 +1,12 @@
+﻿using _01_WineProject.Business.DTOs;
+using _03_WineProject.Domain.Entities;
+
+namespace _01_WineProject.Business.Interfaces
+{
+    public interface IProdutoService : IService<Produto>
+    {
+        Task<ProdutoDTO> CriarProduto(ProdutoDTO produtoDTO);  
+        Task<bool> RemoverProduto(Guid id);
+        Task<ProdutoDTO> AtualizarProduto(ProdutoDTO produtoDTO);
+    }
+}
