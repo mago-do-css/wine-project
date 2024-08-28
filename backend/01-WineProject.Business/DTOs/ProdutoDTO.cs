d4b656d6dbd91fc1d8c48539374afd931c9dd0f7
Iago Melo <iagooliveiradev@outlook.com>
2024-08-28 01:10:09 -0300

    feat: dto do produto

---
+++ backend/01-WineProject.Business/DTOs/ProdutoDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_WineProject.Business.DTOs
+{
+    public class ProdutoDTO
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public int QtdeEstoque { get; set; }
+        public decimal Preco { get; set; }
+        public float Peso { get; set; }
+        public string? Marca { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
