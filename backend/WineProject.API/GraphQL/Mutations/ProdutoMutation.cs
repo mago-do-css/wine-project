754eb6da99c07c7fbd256b99908b30c8fbf373c6
Iago Melo <141418614+mago-do-css@users.noreply.github.com>
2024-08-28 19:48:20 -0300

    build: Ajustes de estrutura e .gitignore

---
+++ backend/WineProject.API/GraphQL/Mutations/ProdutoMutation.cs
@@ -0,0 +1,32 @@
+﻿using _01_WineProject.Business.DTOs;
+using _01_WineProject.Business.Services;
+using HotChocolate;
+using System.Threading.Tasks; 
+
+public class ProdutoMutation
+{
+    private readonly ProdutoService _produtoService;
+
+    public ProdutoMutation(ProdutoService produtoService)
+    {
+        _produtoService = produtoService; 
+    }
+
+    public async Task<ProdutoDTO> AdicionarProduto(ProdutoDTO produtoDTO)
+    {
+       return await _produtoService.CriarProduto(produtoDTO);
+    }
+
+    public async Task<ProdutoDTO> AtualizarProduto(ProdutoDTO produtoDTO)
+    {
+
+        return await _produtoService.AtualizarProduto(produtoDTO);
+    }
+
+    public async Task<bool> ExcluirProduto(Guid id)
+    {
+         var response = await _produtoService.RemoverProduto(id);
+
+        return response;
+    }
+}
