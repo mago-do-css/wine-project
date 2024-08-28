efa943618f1448102497ad3bb1be1c51ac1e6cc3
Iago Melo <iagooliveiradev@outlook.com>
2024-08-28 01:19:01 -0300

    feat: interface do repostório

---
+++ backend/02-WineProject.Data/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using _03_WineProject.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace _01_WineProject.Business.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        Task Criar(TEntity entity);
+        Task<TEntity> BuscarPorId(Guid id);
+        Task<List<TEntity>> BuscarTodos();
+        Task<bool> Remover(Guid id);
+        Task<bool> Atualizar(TEntity entity); 
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+    }
+}
