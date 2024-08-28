6784098aa40eed83e3474c5db61448aa44799ece
Iago Melo <iagooliveiradev@outlook.com>
2024-08-28 01:14:50 -0300

    feat: criação da entidade produto

---
+++ backend/03-WineProject.Domain/Entities/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace _03_WineProject.Domain.Entities
+{
+    public class Produto
+    {
+        public Guid Id { get; set; }
+        public string? Nome { get; set; }
+        public int QtdeEstoque { get; set; }
+        public decimal Preco { get; set; }
+        public float Peso { get; set; } 
+        public string? Marca { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
