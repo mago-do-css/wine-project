886693ff7db2307498aed7601d500bcbe19c520c
Iago Melo <iagooliveiradev@outlook.com>
2024-08-28 01:19:23 -0300

    wip: interface do produto repositório

---
+++ backend/02-WineProject.Data/Interfaces/IProdutoRepository.cs
@@ -0,0 +1,9 @@
+﻿using _03_WineProject.Domain.Entities;
+
+namespace _01_WineProject.Business.Interfaces
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+       //Task<Produto> BuscarProdutoPorId(Guid id);
+    }
+}
