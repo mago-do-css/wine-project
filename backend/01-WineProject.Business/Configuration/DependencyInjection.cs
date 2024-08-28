98c67bf45f475e3c485f0651db34c33ca1ae3f03
Iago Melo <iagooliveiradev@outlook.com>
2024-08-28 01:11:39 -0300

    feat: configurado injeção de dependência

---
+++ backend/01-WineProject.Business/Configuration/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using _01_WineProject.Business.Interfaces;
+using _01_WineProject.Business.Services;
+using _02_WineProject.Data.Context;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _01_WineProject.Business.Configuration
+{
+    public static class DependencyInjection
+    { 
+        public static IServiceCollection DependencesServices(this IServiceCollection services)
+        { 
+            services.AddScoped<IProdutoService, ProdutoService>(); 
+            
+            return services;
+        } 
+    }
+}
