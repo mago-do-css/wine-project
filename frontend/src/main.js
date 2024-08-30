a3fb50b7e6456f0ab50b3a919a552901a15b1ce9
Iago Melo <iagooliveiradev@outlook.com>
2024-08-30 05:50:19 -0300

    feat: condigurando o apollo client

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp, h, provide } from 'vue';
+import './css/app.css';
+import App from './App.vue';
+import { ApolloClient } from '@apollo/client';
+import { DefaultApolloClient } from '@vue/apollo-composable';
+import PrimeVue from 'primevue/config';
+import { useRouter } from 'vue-router';
+
+const app = createApp({
+    setup(){
+        provide(DefaultApolloClient, apolloClient);
+    },
+    render: () => h(App),
+});
+
+app.use(PrimeVue);
+app.use(useRouter);
 
 createApp(App).mount('#app')
