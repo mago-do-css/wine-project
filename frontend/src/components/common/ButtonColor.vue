a5195794edefb24da5a07fd5990f8dcc2a28b9d0
Iago Melo <iagooliveiradev@outlook.com>
2024-08-30 05:47:46 -0300

    feat: criando componentes com props

---
+++ frontend/src/components/common/ButtonColor.vue
@@ -0,0 +1,24 @@
+<template> 
+  <button :style="{ backgroundColor: color}" class="py-2 px-5 font-medium text-[16px] rounded-full transition hover:bg-[#293D33] ">
+    {{ text }}
+  </button>
+</template>
+
+<script> 
+
+export default {
+  name: 'ButtonColor', 
+  props:{
+    text:{
+        type: String, 
+    },  
+    color: {
+      type: String,
+    },  
+  },
+}
+</script>
+
+<style>
+ 
+</style>
---
+++ frontend/src/components/common/Card.vue
@@ -0,0 +1,59 @@
+<template> 
+    <div class="flex mb-4">
+        <div class="rounded-[11px] bg-white h-[100px] w-[100px] mr-5"> 
+            <img class="rounded-full" src="../../assets/images/product.webp" alt="" />
+        </div>
+        <div class="flex flex-col justify-between">
+            <div class="flex items-baseline">
+                <div>
+                    <h2 class="text-[20px] font-semibold">{{ nome }}</h2>
+                    <p>{{ descricao }}</p>
+                </div>
+                <h3 class="font-medium">{{ estoque }}x</h3>
+            </div>
+            <div>
+                <button @click="handleclick" 
+                        class="font-semibold underline mr-10 transition text-[#293D33] hover:text-[#44BB7F]">Editar</button>
+                <button class="font-semibold underline text-[#BE0707] hover:text-[#ff0b0b] transition">Remover</button>
+            </div>
+        </div> 
+    </div>
+</template>
+
+<script> 
+
+export default {
+  name: 'Card', 
+  props:{
+    nome:{
+        type: String,
+        default: 'Nome do produto'
+    },
+    description:{
+        type: String,
+        default: 'Lorem ispum dolor sit amet'
+    },
+    estoque:{
+        type: Number,
+        default:10
+    },
+    peso:{
+        type: Number,
+        default:0.5
+    },
+    marca:{
+        type: String,
+        default:'Marca'
+    }
+  },
+  methods:{
+    handleclick(){
+        this.$emit('edit');
+    }
+  }
+}
+</script>
+
+<style>
+ 
+</style>
