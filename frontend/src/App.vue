0148421f8a6f69ada7a13cba6848f8ba7c1b959b
Iago Melo <iagooliveiradev@outlook.com>
2024-08-30 05:49:40 -0300

    feat: redirecionando para pagina do componente

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,32 @@
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
+<template> 
+    <component :is="currentPage"></component> 
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
+
+<script>
+import { ref } from 'vue';
+import { routes } from '../src/routes/routes'; 
+
+export default {
+  name: 'App',
+  setup() {
+    const currentPage = ref(routes[window.location.pathname] || routes['/']);
+
+    function navigate(path) {
+      window.history.pushState({}, '', path);
+      currentPage.value = routes[path] || routes['/'];
+    }
+ 
+    window.addEventListener('popstate', () => {
+      currentPage.value = routes[window.location.pathname] || routes['/'];
+    });
+
+    return {
+      currentPage,
+      navigate
+    };
+  }
+};
+</script>
